f2536010fca126cfe72316c4b83a0ad8d1b8314d
mortinso <89762267+mortinso@users.noreply.github.com>
2024-06-06 15:10:35 +0100

    ex01: Improved main.cpp
    Still need to add tests to check if brain copy is a deep copy

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 15:46:56 by mortins-          #+#    #+#             */
-/*   Updated: 2024/06/05 17:03:58 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:10:07 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,58 @@
 #include "../inc/Dog.hpp"
 
 int main() {
-	{
+	{	// Test an array of animals.
+		// To check that it creates the right number of members: Comment the other tests and run `./brainiacs | grep <class_name>`
 		std::cout << INVERT << "Array of animals:" << RESET << std::endl;
 		int				size = 6;
 		const Animal*	animals[size];
 
+		// Add half dogs and half cats to the animals array
 		for (int i = 0; i < (size / 2); i++)
 			animals[i] = new Dog();
 		for (int i = (size / 2); i < size; i++)
 			animals[i] = new Cat();
+		std::cout << std::endl;
 
 		// All animals make sound
 		for (int i = 0; i < size; i++)
 			animals[i]->makeSound();
 
+		std::cout << std::endl;
 		for (int i = 0; i < size; i++)
 			delete animals[i];
 	}
 	std::cout << std::endl << std::endl;
-	{
-		std::cout << INVERT << "Ideas overlap:" << RESET << std::endl;
+	
+	{	// Test that ideas are overwritten when the brain is full
+		std::cout << INVERT << "Ideas overwrite:" << RESET << std::endl;
 		const Animal* cat = new Cat();
 
+		// Add 100 ideas to cat
 		for (int i = 0; i < 100; i++)
 			cat->newIdea("Eureka!");
 		cat->shareIdeas();
-		std::cout << "More ideas!" << std::endl;
+		
+		std::cout << std::endl;
+		// Add 3 extra ideas to cat to check that it doesn't segfault
 		for (int i = 0; i < 3; i++)
 			cat->newIdea("New Eureka!");
 		cat->shareIdeas();
+		
 		delete cat;
 	}
-	// test animal brain
+	std::cout << std::endl << std::endl;
+	
+	{	// Check that animal has no brain, and therefore can't have ideas
+		std::cout << INVERT << "Animal ideas:" << RESET << std::endl;
+		const Animal* animal = new Animal();
+
+		animal->newIdea("Eureka!");
+		animal->shareIdeas();
+
+		delete animal;
+	}
+	
 	// check if deep copy
 	// add more tests
 }
